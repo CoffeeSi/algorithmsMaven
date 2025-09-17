5fa3709ad91e2bd86bbce67928c9d50370e4ace2
CoffeeSi <ewgenik02032006@gmail.com>
2025-09-17 12:56:38 +0500

    feat(quicksort): smaller-first recursion, randomized pivot + tests

---
+++ src/main/java/com/coffeesi/QuickSort.java
@@ -0,0 +1,47 @@
+package com.coffeesi;
+
+import java.util.Random;
+
+public class QuickSort {
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length < 2) return;
+        quickSort(arr, 0, arr.length-1);
+    }
+
+    public static int partition(int[] arr, int l, int r) {
+        Random rand = new Random();
+        
+        int pivotIndex = rand.nextInt(r - l + 1) + l;
+        int pivot = arr[pivotIndex];
+        swap(arr, pivotIndex, r);
+
+        int i = l - 1;
+        for (int j = l; j < r; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);     
+            }
+        }
+        swap(arr, i+1, r);
+        return i+1;
+    }
+
+    public static void quickSort(int[] arr, int l, int r) {
+        while (l < r) {
+            int part = partition(arr, l, r);
+            if (part - l < r - part) {
+                quickSort(arr, l, part - 1);
+                l = part + 1;
+            } else {
+                quickSort(arr, part + 1, r);
+                r = part - 1;
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
---
+++ src/test/java/com/coffeesi/QuickSortTest.java
@@ -0,0 +1,52 @@
+package com.coffeesi;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.Random;
+import java.util.Arrays;
+import org.junit.*;
+
+public class QuickSortTest {
+
+    @Test
+    public void testEmptyArray() {
+        int[] actuals = {};
+        QuickSort.sort(actuals);
+        assertArrayEquals(new int[]{}, actuals);
+    }
+
+    @Test
+    public void testSingleElement() {
+        int[] actuals = {42};
+        QuickSort.sort(actuals);
+        assertArrayEquals(new int[]{42}, actuals);
+    }
+
+    @Test
+    public void testSortedArray() {
+        int[] actuals = {1, 2, 3, 4, 5};
+        QuickSort.sort(actuals);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, actuals);
+    }
+
+    @Test
+    public void testNegativeNumbers() {
+        int[] actuals = {-20, 23, -12, 3, 0, -43, 43};
+        int[] expecteds = Arrays.copyOf(actuals, actuals.length);
+        QuickSort.sort(actuals);
+    }
+
+    @Test
+    public void test100randomNumbers() {
+        Random rand = new Random();
+        int[] actuals = new int[100];
+        for (int i = 0; i < actuals.length; i++) {
+            actuals[i] = rand.nextInt(1000) - 500;
+        }
+        int[] expecteds = Arrays.copyOf(actuals, actuals.length);
+        Arrays.sort(expecteds);
+
+        QuickSort.sort(actuals);
+        assertArrayEquals(expecteds, actuals);
+    }
+}
