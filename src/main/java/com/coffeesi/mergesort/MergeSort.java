7c282b25716464797659c3c32224a30f61530624
CoffeeSi <ewgenik02032006@gmail.com>
2025-09-26 03:21:24 +0500

    feat(merge): add metrics

---
+++ src/main/java/com/coffeesi/mergesort/MergeSort.java
@@ -0,0 +1,68 @@
+package com.coffeesi.mergesort;
+
+import com.coffeesi.metrics.Metrics;
+import com.coffeesi.util.Utils;
+
+public class MergeSort {
+    private static final int CUTOFF_THRESHOLD = 16;
+
+    public static void sort(int[] arr, Metrics metrics) {
+        Utils.guardNotNull(arr);
+        metrics.addAllocations();
+        int[] buffer = new int[arr.length];
+        metrics.startTimer();
+        mergeSort(arr, buffer, 0, arr.length - 1, metrics);
+        metrics.stopTimer();
+        metrics.writeToCSV("MergeSort");
+    }
+
+    private static void mergeSort(int[] arr, int[] buffer, int l, int r, Metrics metrics) {
+        if (l >= r) return;
+        metrics.enterRec();
+        if (r - l + 1 <= CUTOFF_THRESHOLD) {
+            insertionSort(arr, l, r, metrics);
+            metrics.exitRec();
+            return;
+        }
+
+        int m = (l + r)/2;
+        mergeSort(arr, buffer, l, m, metrics);
+        mergeSort(arr, buffer, m + 1, r, metrics);
+
+        merge(arr, buffer, l, m, r, metrics);
+        metrics.exitRec();
+    }
+
+    private static void merge(int[] arr, int[] buffer, int l, int m, int r, Metrics metrics) {
+        for (int i = l; i <= r; i++)
+            buffer[i] = arr[i];
+
+        int i = l;
+        int j = m + 1;
+        int k = l;
+
+        while (i <= m && j <= r) {
+            metrics.addComparisons();
+            if (buffer[i] <= buffer[j])
+                arr[k++] = buffer[i++];
+            else
+                arr[k++] = buffer[j++];
+        }
+
+        while (i <= m)
+            arr[k++] = buffer[i++];
+    }
+
+    private static void insertionSort(int[] arr, int l, int r, Metrics metrics) {
+        for (int i = l + 1; i <= r; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= l && arr[j] > key) {
+                metrics.addComparisons();
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[++j] = key;
+        }
+    }
+}
--- src/test/java/com/coffeesi/mergesort/MergeSortTest.java
+++ src/test/java/com/coffeesi/mergesort/MergeSortTest.java
@@ -1,4 +1,4 @@
-package com.coffeesi;
+package com.coffeesi.mergesort;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertThrows;
@@ -7,37 +7,46 @@ import java.util.Random;
 import java.util.Arrays;
 import org.junit.*;
 
+import com.coffeesi.metrics.Metrics;
+
 public class MergeSortTest {
 
     @Test
     public void testEmptyArray() {
+        Metrics m = new Metrics("test");
         assertThrows(IllegalArgumentException.class, 
-                () -> MergeSort.sort(new int[]{}));
+                () -> MergeSort.sort(new int[]{}, m));
     }
 
     @Test
     public void testSingleElement() {
+        Metrics m = new Metrics("test");
         int[] actuals = {42};
-        MergeSort.sort(actuals);
+        MergeSort.sort(actuals, m);
         assertArrayEquals(new int[]{42}, actuals);
     }
 
     @Test
     public void testSortedArray() {
+        Metrics m = new Metrics("test");
         int[] actuals = {1, 2, 3, 4, 5};
-        MergeSort.sort(actuals);
+        MergeSort.sort(actuals, m);
         assertArrayEquals(new int[]{1, 2, 3, 4, 5}, actuals);
     }
 
     @Test
     public void testNegativeNumbers() {
+        Metrics m = new Metrics("test");
         int[] actuals = {-20, 23, -12, 3, 0, -43, 43};
         int[] expecteds = Arrays.copyOf(actuals, actuals.length);
-        MergeSort.sort(actuals);
+        Arrays.sort(expecteds);
+        MergeSort.sort(actuals, m);
+        assertArrayEquals(expecteds, actuals);
     }
 
     @Test
     public void test100randomNumbers() {
+        Metrics m = new Metrics("test5");
         Random rand = new Random();
         int[] actuals = new int[100];
         for (int i = 0; i < actuals.length; i++) {
@@ -46,7 +55,7 @@ public class MergeSortTest {
         int[] expecteds = Arrays.copyOf(actuals, actuals.length);
         Arrays.sort(expecteds);
 
-        MergeSort.sort(actuals);
+        MergeSort.sort(actuals, m);
         assertArrayEquals(expecteds, actuals);
     }
 }
